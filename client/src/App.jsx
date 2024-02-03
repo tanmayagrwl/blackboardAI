88953ff8f01914d137bfa57622ec0d4e30243376
Jatin Kumar <main.jatink@gmail.com>
2024-02-03 19:16:23 +0530

    init client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <>
+    <div>
       <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
