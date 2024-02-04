c6322eb79547c4264ce548ba0fb98c16713a89a1
tanmayagrwl <tanmayhp78@gmail.com>
2024-02-04 13:07:42 +0530

    feat: complete login page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import Login from './Components/Login';
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <Login />
     </div>
   );
 }
